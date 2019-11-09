1f954357660c694aa34c82fad9a2641a490e6130
180107027ayanmiram <55638336+180107027ayanmiram@users.noreply.github.com>
2019-11-09 15:32:09 +0600

    dobavil func-on dlya knopok
    4,5,6

--- script.js
+++ script.js
@@ -2,6 +2,19 @@
 function putInThree(){
 	document.calculator.textfield.value += '3';
 }
+//4-ty kosady
+function putInFour(){
+	document.calculator.textfield.value += '4';
+}
+//5-ty kosady
+function putInFive(){
+	document.calculator.textfield.value += '5';
+}
+//6-ny kosady
+function putInSix(){
+	document.calculator.textfield.value += '6';
+}
+
 function putInEight(){
 	document.calculator.textfield.value += '8';
 }

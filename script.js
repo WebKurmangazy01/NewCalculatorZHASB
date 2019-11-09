ed86ccc947388d7b03c0288b38ee93a06c64c1b3
180107180 <49221685+180107180@users.noreply.github.com>
2019-11-09 15:33:31 +0600

    Uploaded functions for 1,2,9 ciphers
    Upload1

--- script.js
+++ script.js
@@ -2,19 +2,6 @@
 function putInThree(){
 	document.calculator.textfield.value += '3';
 }
-//4-ty kosady
-function putInFour(){
-	document.calculator.textfield.value += '4';
-}
-//5-ty kosady
-function putInFive(){
-	document.calculator.textfield.value += '5';
-}
-//6-ny kosady
-function putInSix(){
-	document.calculator.textfield.value += '6';
-}
-
 function putInEight(){
 	document.calculator.textfield.value += '8';
 }
@@ -22,4 +9,16 @@ function putInSeven(){
 	document.calculator.textfield.value += '7';
 }function putInTZero(){
 	document.calculator.textfield.value += '0';
+}
+//This function putting 1 in a textfield on button onclick event
+function putInOne(){
+	document.calculator.textfield.value += '1';
+}
+//This function putting 2 in a textfield on button onclick event
+function putInTwo(){
+	document.calculator.textfield.value += '2';
+}
+//This function putting 9 in a textfield on button onclick event
+function putInNine(){
+	document.calculator.textfield.value += '9';
 }

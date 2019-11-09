d868a93875346ad02a65d62f6350ae6acc7c21a6
Sultan-Bibarys <57250403+Sultan-Bibarys@users.noreply.github.com>
2019-11-09 15:28:49 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+/*SB*/
+function putInThree(){
+	document.calculator.textfield.value += '3';
+}
+function putInEight(){
+	document.calculator.textfield.value += '8';
+}
+function putInSeven(){
+	document.calculator.textfield.value += '7';
+}function putInTZero(){
+	document.calculator.textfield.value += '0';
+}
